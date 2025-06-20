a9a079a6fc5fa5cedf6a7939d4082bb356cef659
Enrico Marazzi <enrico.marazzi@uclouvain.be>
2025-06-20 12:07:46 +0200

    Commenting and typing (begin)

--- pp/flows/generate_ion.py
+++ pp/flows/generate_ion.py
@@ -1,49 +1,73 @@
 from pymatgen.core import Structure
-from dataclasses import dataclass, field
-from jobflow import job, Flow, Maker
+from dataclasses import dataclass
+from jobflow import Flow, Maker, Job
 from atomate2.siesta.jobs.core import StaticMaker
 from pathlib import Path
 from glob import glob
+from typing import Union, List
 import os
 from pp.utils import cp_ion
 
-@dataclass 
+@dataclass
 class GenerateIons(Maker):
     """
     Maker to generate the database of the ion files from siesta 
     from a specific set of pseudos
 
+    Args:
+        name: str
+            Name of the Flow
+
 
     """
-    name = "Generate Ion Files"
+    name: str = "Generate Ion Files"
 
     def make(
         self,
-        pseudo_psml_dir: str | Path,
-        save_ion_folder: str | Path,
-        database_folder: str | Path,
+        pseudo_psml_dir: Union[str, Path],
+        save_ion_folder: Union[str, Path],
+        database_folder: Union[str, Path],
     ) -> Flow:
         """
-        Maker to generate all the siesta jobs, collect the ion files and 
-        copy them in a save directory
+        Generate all the SIESTA jobs, collect the ion files, and
+        copy them to a save directory
+
+        Args:
+            pseudo_psml_dir: Directory containing pseudos files (.psml)
+            save_ion_folder: Directory where ion files will be saved
+            database_folder: Directory containing CIF files for structures
+
+        Returns:
+            A Flow
         """
+        jobs: List[Job] = []
 
-        jobs = []
+        # Find all pseudopotential files in the specified directory
+        pseudos: List[str] = glob(os.path.join(os.path.abspath(pseudo_psml_dir),'*psml'))
 
-        pseudos = glob(os.path.join(os.path.abspath(pseudo_psml_dir),'*psml'))
+        # Convert database_folder to abs path
         database_folder = os.path.abspath(database_folder)
 
         for pseudo in pseudos:
-            element  = pseudo.split('/')[-1].split('.')[0]
-            if os.path.exists(os.path.join(database_folder,f"{element}.cif")):
-                structure = Structure.from_file(os.path.join(database_folder,f'{element}.cif'))
-                job_ion = StaticMaker().make(structure=structure)
+            # Extract the element name from the pseudopotential file name
+            element: str = pseudo.split('/')[-1].split('.')[0]
+
+            # Check if a corresponding CIF file exists for the element
+            cif_path: str = os.path.join(database_folder, f"{element}.cif")
+            if os.path.exists(cif_path):
+                structure: Structure = Structure.from_file(cif_path)
+                job_ion: Job = StaticMaker().make(structure=structure)
                 
                 job_ion.name = element
                 
                 jobs.append(job_ion)
-        job_copy = cp_ion(outdirs = [job.output for job in jobs],ion_dir = save_ion_folder)
+
+        # Create a job to copy the ion files to the save directory
+        job_copy: Job = cp_ion(
+            outdirs = [job.output for job in jobs],
+            ion_dir = save_ion_folder
+        )
         jobs.append(job_copy)
 
         return Flow(jobs=jobs,output = job_copy.output,name = self.name)
-        
+
--- pp/flows/prepare_dataset.py
+++ pp/flows/prepare_dataset.py
@@ -3,12 +3,9 @@ from dataclasses import dataclass, field
 from jobflow import Maker, job, Flow, Job
 from pathlib import Path
 from pp.jobs.mod_structure import generate_training_population
-from pp.jobs.jobs import QEscf, QEpw2bgw, QEpw2bgw
+from pp.jobs.jobs import QEscf, QEpw2bgw
 from pp.jobs.hpro import HPROWrapper
-
-from typing import List, Tuple, Union
-
-
+from typing import List, Tuple, Union, Optional
 import os
 
 __all__ = [
@@ -18,78 +15,120 @@ __all__ = [
 @dataclass
 class GenerateDFTData(Maker):
     """
-    
+    A Maker to generate the training dataset from dft PW computations
+    and siesta ion files
+
+    Args:
+        name: str
+              Name of the flows
+        structures_dir: Union[str, Path]
+              Directory to store the structures cif files
+        ao_hamiltonian_dir: Union[str, Path]
+              Directory to store the preprocessed data from HPRO
+        distance: float
+              Distance in angstroms by which to perturb each site.
+        min_distance: float
+              if None, all displacements will be equal amplitude. 
+              If int or float, perturb each site a distance drawn 
+              from the uniform distribution between 'min_distance' and 'distance'.
+        supercell_size: Union[List[int], Tuple[int, ...]]
+              A scaling matrix for transforming the lattice
+              vectors. Has to be all integers
+        training_size: int
+              Size of the training dataset (Default:500)
+        include_vacances: bool
+              Whether to include structure with vacances in your structure database.
+              Still not implemented.
+        qe_run_cmd: str
+              Command to execute pw.x
+        fname_pwi_template: str
+              Path to the template for pw scf calculations
+        upf_dir: Union[str, Path]                                                                       
+              Directory containing the upf pseudos for QE.
+              Default is the environment variable $ESPRESSO_PSEUDO
+        ecutwfn: Union[int, float]
+              ecutwfn variable from QE, used only for HPRO.
+              To set a specific value of ecutwfn in your QE calculation
+              modify the scf template.
+        num_qe_workers: Optional[int] = None
+              Number of workers to execute pw.x calculations.
+              Default to None that corresponds to one worker per structure
+        pw2bgw_run_cmd: str
+              As qe_run_cmd but for pw2bgw.x
+        fname_pw2bgw_template: str
+              As fname_pwi_template but for pw2bgw.x
+        ion_dir: Union[str, Path]
+              Directory containing the ion files generated with siesta
     """
     name: str = "GenerateDFTData"
-    
-    structures_dir: str | Path = './'
-    ao_hamiltonian_dir: str | Path = './'
+    structures_dir: Union[str, Path] = './'
+    ao_hamiltonian_dir: Union[str, Path] = './'
     distance: float = 0.1
-    supercell_size: List | Tuple = field(default_factory=lambda: [1, 1, 1])
-    min_distance: float | None = None
+    min_distance: Optional[float,int] = 0.001
+    supercell_size: Union[List[int], Tuple[int, ...]] = field(default_factory=lambda: [1, 1, 1])
     training_size: int = 500
     include_vacancies: bool = False
 
     qe_run_cmd: str = "srun --mpi=cray_shasta $PATHQE/bin/pw.x"
     fname_pwi_template: str = "scf.in"
-    upf_dir: str | Path = os.getenv('ESPRESSO_PSEUDO','./')
-    ecutwfn: int | float = 30.0
-    num_qe_workers: int | None = None
-    pw2bgw_command: str = "srun --mpi=cray_shasta $PATHQE/bin/pw2bgw.x"
+    upf_dir: Union[str, Path] = os.getenv('ESPRESSO_PSEUDO', './')
+    ecutwfn: Union[int, float] = 30.0
+    num_qe_workers: Optional[int] = None
+    pw2bgw_run_cmd: str = "srun --mpi=cray_shasta $PATHQE/bin/pw2bgw.x"
     fname_pw2bgw_template: str = "pw2bgw.in"
-    ion_dir: str | Path = './'
+    ion_dir: Union[str, Path] = './'
 
     def make(
         self,
         structure: Structure,
-        ):
+    ) -> Flow:
         """
-        Create the flow to generate DFT data
-        
-        """
+        Create the flow to generate the training dataset.
 
-        jobs = []
+        Args:
+            structure: Structure
+                     The input structure
+
+        Returns:
+            A Flow
+        """
+        jobs: List[Job] = []
 
         gen_structures_job = generate_training_population(
             structure = structure,
-            structures_dir=self.structures_dir,
+            structures_dir = self.structures_dir,
             distance = self.distance, 
             supercell_size = self.supercell_size,
             min_distance = self.min_distance, 
-            size =  self.training_size, 
-            #include_vacancies =  self.include_vacancies
-            )
+            size =  self.training_size,     
+        )
         jobs.append(gen_structures_job)
-        
 
-
-        qe_run_jobs = QEscf(    
+        qe_run_jobs = QEscf(
             qe_run_cmd = self.qe_run_cmd,
             num_qe_workers = self.num_qe_workers,
             fname_pwi_template = self.fname_pwi_template,
             fname_structures = gen_structures_job.output
         )
-        
         jobs.append(qe_run_jobs)
 
         pw2bgw_run_jobs = QEpw2bgw(
             scf_outdir = qe_run_jobs.output,
             name = 'Pw2Bgw Labelling',
-            pw2bgw_command=self.pw2bgw_command,
-            fname_pw2bgw_template=self.fname_pw2bgw_template,
+            pw2bgw_command = self.pw2bgw_run_cmd,
+            fname_pw2bgw_template = self.fname_pw2bgw_template,
             num_workers = 1
         )
         jobs.append(pw2bgw_run_jobs)
 
         hpro_job = HPROWrapper(
-            qe_run_output = qe_run_jobs.output,
+            qe_run_output = pw2bgw_run_jobs.output,
             ion_dir = self.ion_dir,
             ao_hamiltonian_dir = self.ao_hamiltonian_dir,
             upf_dir = self.upf_dir,
             ecutwfn = self.ecutwfn,
-            metadata = {'pw2bgw_completed':pw2bgw_run_jobs.output}
         )
 
         jobs.append(hpro_job)
 
-        return Flow(jobs,output=[j.output for j in jobs],name=self.name)
+        return Flow(jobs, output = [j.output for j in jobs], name=self.name)
