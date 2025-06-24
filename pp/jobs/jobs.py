815e4885d542109118d803bbed5ac64ff0e24259
Enrico Marazzi <enrico.marazzi@uclouvain.be>
2025-06-24 09:23:09 +0200

    Add kpoints sampling

--- pp/jobs/jobs.py
+++ pp/jobs/jobs.py
@@ -6,16 +6,12 @@ credits to Alberto Pacini
 
 from dataclasses import field
 from jobflow import job, Flow, Response
-from pp.jobs.labelling import QEstaticLabelling, QEpw2bgwLabelling , QEnscfLabelling, QEbandLabelling
+from pp.jobs.labelling import QEstaticLabelling, QEpw2bgwLabelling , QEnscfLabelling, QEbandLabelling, qe_params_from_config
 import numpy as np
 
 @job
 def QEscf(
-    name: str = "do_qe_static_labelling",
-    qe_run_cmd: str = "mpirun -np 1 pw.x",
-    num_qe_workers: int | None = 1, #Number of workers to use for the calculations. If None setp up 1 worker per scf
-    fname_pwi_template: str | None = None, #Path to file containing the template QE input
-    fname_structures: str | None = None, #Path to ASE-readible file containing the structures to be computed
+    params: dict | None = None,
 ):
     """
     Initialize the QEScfLabelling with the provided parameters.
@@ -25,17 +21,11 @@ def QEscf(
     kwargs: dict
         Dictionary containing the parameters for the QEScfLabelling.
     """
-    #Parameters for QEScfLabelling
-    qe_params = {
-        "name": name,
-        "qe_run_cmd": qe_run_cmd,
-        "num_qe_workers": num_qe_workers,
-        "fname_pwi_template": fname_pwi_template,
-        "fname_structures": fname_structures,
-    }
+    #Initialize QEstaticLabelling with the provided parameters
+    qe_params = qe_params_from_config(params)    
 
     # Execute QE static labelling
-    # and return the paths to the labelled structures
+    # return a list containing (list[success], list[pwo], list[outdir]) for each worker
     output_per_worker = QEstaticLabelling(**qe_params).make()
 
     return output_per_worker
